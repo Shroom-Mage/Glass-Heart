70e1e5a57a62fc5228b0a8653b045df56f1515c2
Van Phelan <vephelan@gmail.com>
2021-03-06 16:32:04 -0600

    Improved spell effects and reorganized player and caster behaviors

--- Assets/Scripts/SpellEffectBehavior.cs
+++ Assets/Scripts/SpellEffectBehavior.cs
@@ -5,15 +5,44 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class SpellEffectBehavior : MonoBehaviour
 {
+    public float Damage = 0.0f;
+    public float Stun = 0.0f;
+    public float Knockback = 0.0f;
+    public float Duration = 1.0f;
+    public bool EndOnCollision = false;
+    public Vector3 Velocity = new Vector3();
+
+    private float _timer = 0.0f;
+    private Collider _collider;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        _collider = GetComponent<Collider>();
+        _collider.isTrigger = true;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // Move forward
+        transform.Translate(Velocity * Time.deltaTime, Space.Self);
+
+        // Destroy self if duration has expired
+        if (_timer >= Duration) {
+            // Play particle effect here
+            Destroy(gameObject);
+        }
+        _timer += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Hit!");
+        MonsterBehavior monster = other.gameObject.GetComponent<MonsterBehavior>();
+
+        monster.transform.Translate(transform.forward * Knockback);
+
+        // Destroy self if needed
+        if (EndOnCollision) {
+            Destroy(gameObject);
+        }
     }
 }
