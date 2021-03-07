1b5a27ddb9e6904fb52e5bd641080d17274179bc
Van Phelan <vephelan@gmail.com>
2021-03-07 02:11:10 -0600

    Monster AI, health, mana, and more spell improvements

--- Assets/Scripts/CasterBehavior.cs
+++ Assets/Scripts/CasterBehavior.cs
@@ -6,27 +6,74 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class CasterBehavior : MonoBehaviour
 {
+    public float Health = 100.0f;
+    public float Mana = 100.0f;
+
     public SpellBehavior Spell1;
     public SpellBehavior Spell2;
     public SpellBehavior Spell3;
 
+    private float _maxHealth;
+    private float _maxMana;
+    private float _stun;
+
+    private SpellBehavior _spellCasting;
+
     private NavMeshAgent _agent;
     private float _agentSpeed;
 
+    private void Awake() {
+    }
+
     // Start is called before the first frame update
-    void Start() {
+    private void Start() {
         _agent = GetComponent<NavMeshAgent>();
         _agentSpeed = _agent.speed;
+        _maxHealth = Health;
+        _maxMana = Mana;
+    }
+
+    private void Update() {
+        _stun -= Time.deltaTime;
     }
 
     public void StartCast(SpellBehavior spell) {
+        Mana -= spell.Cost;
         _agentSpeed = _agent.speed;
         _agent.speed = 0.0f;
         _agent.velocity = new Vector3();
+        spell.tag = tag;
         spell.Cast(this);
+        _spellCasting = spell;
     }
 
-    public void EndCast(SpellBehavior spell) {
+    public void EndCast() {
         _agent.speed = _agentSpeed;
+        _spellCasting = null;
+    }
+
+    public bool IsCasting() {
+        return _agent.speed == 0.0f;
+    }
+
+    public void TakeDamage(float damage, float stun, Vector3 knockback) {
+        Debug.Log(name + " took " + damage);
+        // Apply effects
+        Health -= damage;
+        _stun = stun;
+        transform.Translate(knockback, Space.World);
+        // Cancel current spell
+        if (_spellCasting) {
+            _spellCasting.Cancel();
+            _spellCasting = null;
+        }
+    }
+
+    public bool IsStunned() {
+        return _stun > 0.0f;
+    }
+
+    public bool IsAlive() {
+        return Health > 0.0f;
     }
 }
--- Assets/Scripts/MonsterBehavior.cs
+++ Assets/Scripts/MonsterBehavior.cs
@@ -1,16 +1,174 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(CasterBehavior))]
 public class MonsterBehavior : MonoBehaviour
 {
+    public enum State {
+        Idling,
+        Wandering,
+        Approaching,
+        Attacking,
+        Stunned,
+        Dead
+    }
+
+    public float IdleTimer = 1.0f;
+    public float AggroRadius = 5.0f;
+    public float AttackRadius = 2.5f;
+
+    private State _state = State.Idling;
+    private float _timer = 0.0f;
+    private Vector3 _wanderDirection = new Vector3();
+
+    private NavMeshAgent _agent;
+    private CasterBehavior _caster;
+    private PlayerBehavior _player;
+
     // Start is called before the first frame update
     void Start() {
-        
+        _agent = GetComponent<NavMeshAgent>();
+        _caster = GetComponent<CasterBehavior>();
+        _player = FindObjectOfType<PlayerBehavior>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        switch (_state) {
+            case State.Idling:
+                Idle();
+                break;
+            case State.Wandering:
+                Wander();
+                break;
+            case State.Approaching:
+                Approach();
+                break;
+            case State.Attacking:
+                Attack();
+                break;
+            case State.Stunned:
+                Stunned();
+                break;
+            case State.Dead:
+                Dead();
+                break;
+        }
+        _timer += Time.deltaTime;
+    }
+
+    public float DistanceToPlayer() {
+        return (transform.position - _player.transform.position).magnitude;
+    }
+
+    private void EnterIdle() {
+        Debug.Log("Idling");
+        _timer = 0.0f;
+        _state = State.Idling;
+    }
+
+    private void Idle() {
+        // Stop
+        _agent.destination = transform.position;
+
+        // Check state change
+        if (!_caster.IsAlive()) {
+            EnterDead();
+        } else if (_caster.IsStunned()) {
+            EnterStunned();
+        } else if (DistanceToPlayer() <= AggroRadius) {
+            EnterApproach();
+        } else if (_timer >= IdleTimer) {
+            EnterWander();
+        }
+    }
+
+    private void EnterWander() {
+        Debug.Log("Wandering");
+        _wanderDirection = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f)).normalized;
+        _timer = 0.0f;
+        _state = State.Wandering;
+    }
+
+    private void Wander() {
+        // Move
+        _agent.destination = transform.position + _wanderDirection;
+
+        // Check state change
+        if (!_caster.IsAlive()) {
+            EnterDead();
+        } else if (_caster.IsStunned()) {
+            EnterStunned();
+        } else if (DistanceToPlayer() <= AggroRadius) {
+            EnterApproach();
+        } else if (_timer >= IdleTimer / 2) {
+            EnterIdle();
+        }
+    }
+
+    private void EnterApproach() {
+        Debug.Log("Approaching");
+        _state = State.Approaching;
+    }
+
+    private void Approach() {
+        // Move
+        _agent.destination = _player.transform.position;
+
+        // Check state change
+        if (!_caster.IsAlive()) {
+            EnterDead();
+        } else if (_caster.IsStunned()) {
+            EnterStunned();
+        } else if (DistanceToPlayer() <= AttackRadius) {
+            EnterAttack();
+        } else if (DistanceToPlayer() >= AggroRadius * 2.0f) {
+            EnterIdle();
+        }
+    }
+
+    private void EnterAttack() {
+        Debug.Log("Attacking");
+        _agent.destination = transform.position;
+        _state = State.Attacking;
+    }
+
+    private void Attack() {
+        // Check state change
+        if (!_caster.IsAlive()) {
+            EnterDead();
+        } else if (_caster.IsStunned()) {
+            EnterStunned();
+        } else if (DistanceToPlayer() >= AttackRadius) {
+            EnterApproach();
+        } else if (!_caster.IsCasting()) {
+            transform.LookAt(_player.transform);
+            _caster.StartCast(_caster.Spell1);
+        }
+    }
+
+    private void EnterStunned() {
+        Debug.Log("Stunned!");
+        _state = State.Stunned;
+    }
+
+    private void Stunned() {
+        if (!_caster.IsAlive()) {
+            EnterDead();
+        } else if (!_caster.IsStunned()) {
+            EnterApproach();
+        }
+    }
+
+    private void EnterDead() {
+        Debug.Log("Dead!");
+        _state = State.Dead;
+    }
+
+    private void Dead() {
+        Destroy(gameObject);
     }
 }
