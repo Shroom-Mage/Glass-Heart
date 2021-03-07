0f1a85e813204c2c268c696bec55eb12dca9bc21
Van Phelan <vephelan@gmail.com>
2021-03-07 15:12:18 -0600

    Added Gates that repel the player and vanish once monsters are cleared

---
+++ Assets/Scripts/GateBehavior.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateBehavior : MonoBehaviour
+{
+    public GameObject MonsterGroup;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!MonsterGroup.GetComponentInChildren<MonsterBehavior>()) {
+            Destroy(gameObject);
+        }
+    }
+}
